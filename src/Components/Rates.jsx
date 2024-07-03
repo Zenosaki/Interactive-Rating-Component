febaaeddd57609d23da91fda74b296eafc7c6dd6
Zenosaki <arselin.chaabanie@gmail.com>
2024-07-03 21:12:12 +0200

    I FIXED THE ICON WHEN CLICKED  ONLY ONE

--- src/Components/Rates.jsx
+++ src/Components/Rates.jsx
@@ -1,22 +1,21 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react'; // Import useState hook if not already imported
-import { TakeRate, getCurrentRate } from '../scripts/rate';
 
 export default function Rate(props) {
-  const [isActive, setIsActive] = useState(getCurrentRate() === props.rate);
+  const { rate, isActive, onClick } = props;
 
   const handleClick = () => {
-    TakeRate(props.rate);
-    setIsActive(true); 
+    onClick(rate);
   };
 
   return (
     <div className={`icon ${isActive ? 'active' : ''}`} onClick={handleClick}>
-      <h4>{props.rate}</h4>
+      <h4>{rate}</h4>
     </div>
   );
 }
 
 Rate.propTypes = {
   rate: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
--- src/Components/Ratings.jsx
+++ src/Components/Ratings.jsx
@@ -1,29 +1,39 @@
-import {PropTypes} from 'prop-types'
-import icon from '../assets/images/icon-star.svg'
-import Rate from './Rates'
-import { getCurrentRate } from '../scripts/rate';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import icon from '../assets/images/icon-star.svg';
+import Rate from './Rates';
+import { getCurrentRate, TakeRate } from '../scripts/rate';
 
-export default function ratings(props){
-  return(
+export default function Ratings(props) {
+  const [currentRate, setCurrentRate] = useState(getCurrentRate());
+
+  const handleRateClick = (rate) => {
+    setCurrentRate(rate);
+    TakeRate(rate);
+  };
+
+  return (
     <div className="rating-container">
       <img src={icon} alt="icon" className="icon" />
       <div className="Rating-Content">
         <h1>How did we do?</h1>
         <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
         <div className="ratings-icons">
-          <Rate rate="1"/>
-          <Rate rate="2"/>
-          <Rate rate="3"/>
-          <Rate rate="4"/>
-          <Rate rate="5"/>
+          {['1', '2', '3', '4', '5'].map((rate) => (
+            <Rate
+              key={rate}
+              rate={rate}
+              isActive={rate === currentRate}
+              onClick={handleRateClick}
+            />
+          ))}
         </div>
-        <button onClick={()=>props.submit(getCurrentRate())}>SUBMIT</button>
+        <button onClick={() => props.submit(getCurrentRate())}>SUBMIT</button>
       </div>
     </div>
-  )
+  );
 }
 
-ratings.propTypes ={
-  submit: PropTypes.func
-}
-
+Ratings.propTypes = {
+  submit: PropTypes.func,
+};
